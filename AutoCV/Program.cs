47ca1d6682b0f07c651edf7aa9f7d8d34f0cf316
Thalles Sampaio <thallessampaio89@gmail.com>
2025-05-24 23:17:48 -0300

    first commit

--- AutoCV/Program.cs
+++ AutoCV/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+class Program 
+{
+    public static void Main()
+    {
+        string remoteUri = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/2025-05/";
+        string fileName = "Cnaes.zip";
+        string dest = @$"G:\\dev\\{fileName}";
+
+        using (WebClient webClient = new WebClient())
+        {
+            Console.WriteLine("Downloading file {0}...", fileName);
+
+            // Descarga el recurso web y guárdalo en el destino
+            webClient.DownloadFile(remoteUri + fileName, dest);
+
+            Console.WriteLine("Successfully downloaded file {0}", fileName);
+        }
+    }
+}
